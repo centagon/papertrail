4a6adc2e6d0d9cf4f92ee43177805f1a5536ba20
Rene Heijnemans <rene@centagon.com>
2019-12-09 19:41:36 +0700

    Add monolog requirement ; update custom udp handler

--- src/CustomSyslogUdpHandler.php
+++ src/CustomSyslogUdpHandler.php
@@ -3,15 +3,21 @@
 namespace StephaneCoinon\Papertrail;
 
 use Monolog\Handler\SyslogUdpHandler;
+use DateTimeInterface;
 
 class CustomSyslogUdpHandler extends SyslogUdpHandler {
 
     protected $hostname;
-    
+
+    private $customDateFormats = array(
+        self::RFC3164 => 'M d H:i:s',
+        self::RFC5424 => \DateTime::RFC3339,
+    );
+
     /**
      * Make common syslog header (see rfc5424)
      */
-    protected function makeCommonSyslogHeader($severity)
+    protected function makeCommonSyslogHeader($severity, DateTimeInterface $datetime): string
     {
         $priority = $severity + $this->facility;
 
@@ -23,16 +29,29 @@ class CustomSyslogUdpHandler extends SyslogUdpHandler {
             $hostname = '-';
         }
 
-        return "<$priority>1 " .
-            $this->getDateTime() . " " .
-            $hostname . " " .
-            $this->ident . " " .
-            $pid . " - - ";
+        if ($this->rfc === self::RFC3164) {
+            $datetime->setTimezone(new \DateTimeZone('UTC'));
+        }
+
+        $date = $datetime->format($this->customDateFormats[$this->rfc]);
+
+        if ($this->rfc === self::RFC3164) {
+            return "<$priority>" .
+                $date . " " .
+                $hostname . " " .
+                $this->ident . "[" . $pid . "]: ";
+        } else {
+            return "<$priority>1 " .
+                $date . " " .
+                $hostname . " " .
+                $this->ident . " " .
+                $pid . " - - ";
+        }
     }
 
     public function setHostName($hostname) {
         $this->hostname = $hostname;
         return $this;
     }
-    
+
 }
